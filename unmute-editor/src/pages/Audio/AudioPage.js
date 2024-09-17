20e9a99bdf377d7325634117b1e7094a5aa18311
STDev-developer <inexus.developer@stdevmail.com>
2024-09-17 13:54:51 +0400

    Inspiration Redirection

--- unmute-editor/src/pages/Audio/AudioPage.js
+++ unmute-editor/src/pages/Audio/AudioPage.js
@@ -55,7 +55,7 @@ export const AudioPage = () => {
       ))}
 
       <div className="flex flex-row justify-center gap-4 mt-16">
-        <Link className="text-white bg-black border border-rose transition duration-200 ease-out focus:outline-none hover:bg-gray-800 focus:ring-4 focus:ring-rose font-medium rounded-lg px-8 py-2.5 cursor-pointer">
+        <Link to={'/inspirations'} className="text-white bg-black border border-rose transition duration-200 ease-out focus:outline-none hover:bg-gray-800 focus:ring-4 focus:ring-rose font-medium rounded-lg px-8 py-2.5 cursor-pointer">
           Get inspiration
         </Link>
 

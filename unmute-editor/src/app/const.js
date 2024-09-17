612cad4fce247f436168cb9b394c93441cf25881
Gor-Hunanyan <hunan.gor@gmail.com>
2024-09-17 16:09:38 +0400

    Inspirations

--- unmute-editor/src/app/const.js
+++ unmute-editor/src/app/const.js
@@ -4,4 +4,4 @@ export const AWS_KEY_ID = "AKIA3C6FMK5BQ5GRQT3P";
 export const AWS_KEY_SECRET = "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";
 export const S3_BUCKET = "unmute-greetings";
 export const S3_REGION = "eu-north-1";
-export const BASE_API_URL = "https://alien-cunning-shark.ngrok-free.app/api"
+export const BASE_API_URL = "http://51.21.94.14/api"

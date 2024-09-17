8e5cc8218010a8afda3329730483be50d48ac767
MichaelHovhannisyanSTDev <michael@stdevmail.com>
2024-09-17 20:45:19 +0400

    temp base url for api

--- unmute-editor/src/api/inspiration.js
+++ unmute-editor/src/api/inspiration.js
@@ -2,6 +2,10 @@ import axios from "axios";
 import { BASE_API_URL } from "../app/const";
 
 export const getInspirations = async () => {
-    const res = await axios.get(`${BASE_API_URL}/inspirations`, {});
+    const res = await axios.get(`${BASE_API_URL}/inspirations`, {
+        headers: {
+            'ngrok-skip-browser-warning': true,
+        }
+    });
     return res.data.data;
 };

e5c2f8643120d0788af4bc2ab2ad2c9b4d4c2bfb
hrantSTDev <152957197+hrantSTDev@users.noreply.github.com>
2024-09-25 20:18:31 +0400

    Add check

--- unmute-editor/src/components/Frame.js
+++ unmute-editor/src/components/Frame.js
@@ -59,13 +59,13 @@ export const Frame = () => {
       isAlreadyRendered.current = true;
       if (unmutes?.length !== 1) {
         const sortedUnmutes = [...unmutes]?.sort((a, b) => {
-          const a_created = a.properties._created;
-          const b_created = b.properties._created;
+          const a_created = a.properties?._created;
+          const b_created = b.properties?._created;
 
-          if (a_created.date !== b_created.date)
-            return new Date(a_created.date) - new Date(b_created.date);
+          if (a_created?.date !== b_created?.date)
+            return new Date(a_created?.date) - new Date(b_created?.date);
 
-          return a_created.index - b_created.index;
+          return a_created?.index - b_created?.index;
         });
         const itemsWithImage = sortedUnmutes.filter(
           (item) => item.properties._images?.length > 0
@@ -156,11 +156,7 @@ export const Frame = () => {
     return (
       <div className="snap-start">
         <div className="relative top-0 flex justify-center mt-16">
-          <img
-            src={frame_image}
-            alt="Frame"
-            className="relative top-0 w-1/2"
-          />
+          <img src={frame_image} alt="Frame" className="relative top-0 w-1/2" />
           <div className="absolute h-full object-cover">
             <div className="flex flex-col items-center justify-center h-full">
               <Loader size={"w-24 h-24"} />

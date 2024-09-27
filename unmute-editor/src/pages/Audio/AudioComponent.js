33a1dcdc9ac2e41b6d9064d4f3fa5909a2387733
hrantSTDev <152957197+hrantSTDev@users.noreply.github.com>
2024-09-27 12:35:24 +0400

    Hide load

--- unmute-editor/src/pages/Audio/AudioComponent.js
+++ unmute-editor/src/pages/Audio/AudioComponent.js
@@ -57,7 +57,7 @@ export const AudioComponent = ({
   const handlePlayAudio = () => {
     if (!audioRef.current) return;
     pauseAllAudios();
-    audioRef.current.load();
+    // audioRef.current.load();
     setActiveAudio((prev) => ({ ...prev, [item.uuid]: true }));
     if (rangeMap?.start)
       audioRef.current.currentTime = rangeMap?.start / 1000 ?? 0;

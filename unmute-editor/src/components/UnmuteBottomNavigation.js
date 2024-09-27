6ab14ff9fd1623ecf2771de7d01904b0f4db6de2
STDev-developer <inexus.developer@stdevmail.com>
2024-09-27 15:09:41 +0400

    Bottom navigation reset

--- unmute-editor/src/components/UnmuteBottomNavigation.js
+++ unmute-editor/src/components/UnmuteBottomNavigation.js
@@ -45,7 +45,7 @@ export const UnmuteBottomNavigation = () => {
     },
   ];
   return (
-    <div className="fixed bottom-100 left-0 z-50 w-full bg-beige-300 border-t border-beige-200 sm:max-w-max sm:p-3 sm:rounded-xl sm:mx-auto sm:left-0 sm:right-0 sm:bottom-4">
+    <div className="fixed bottom-0 left-0 z-50 w-full bg-beige-300 border-t border-beige-200 sm:max-w-max sm:p-3 sm:rounded-xl sm:mx-auto sm:left-0 sm:right-0 sm:bottom-4">
       <div className="grid sm:gap-2 h-full max-w-2xl grid-cols-6 mx-auto font-medium">
         {NAVIGATION.map((item, index) => (
           <Link
